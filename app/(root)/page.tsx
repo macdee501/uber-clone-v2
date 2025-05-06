0c5d7d3ca5b98e6e31c57bb5b5c6b1d156225824
mcdonald <mcdonaldlekhoaba@gmail.com>
2025-05-06 09:07:16 +0200

    Removed First Map

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import Map from "../components/Map";
 import Map2 from "../components/Map2";
 import { MapProvider } from "../providers/map-provider";
 
@@ -8,9 +7,8 @@ export default function Home() {
       <MapProvider>
         <main className="h-screen bg-black flex flex-col">
           <div className="bg-red flex-0.5">
-            {/* <Map/> */}
             <Map2/>
-            
+
           </div>
           <div className="bg-purple-800 flex-1">
             placeholder for buttons
--- app/components/Map2.tsx
+++ app/components/Map2.tsx
@@ -3,17 +3,19 @@ import { APIProvider, Map } from '@vis.gl/react-google-maps'
 import React from 'react'
 
 const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API;
+const mapId = process.env.NEXT_PUBLIC_MAP_ID;
 
 export default function Map2() {
   return (
     <>
-        <APIProvider apiKey="AIzaSyDs9_T2O4JCO1x6ecKSwAy4liFN84Ihi-4">
+        <APIProvider apiKey={apiKey}>
             <Map
                 style={{width:'100vw',height:'50vh'}}
                 defaultCenter={{lat:-26.974673114466885,lng:26.66995356468627}}
                 defaultZoom={15}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
+                mapId={mapId}
             />
 
         </APIProvider>
