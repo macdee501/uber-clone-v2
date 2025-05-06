937ec0ba57c61ca76555d55ab0805b3f3cad8618
mcdonald <mcdonaldlekhoaba@gmail.com>
2025-05-06 09:27:52 +0200

    Removed First Map

--- app/components/Map2.tsx
+++ app/components/Map2.tsx
@@ -2,14 +2,17 @@
 import { APIProvider, Map } from '@vis.gl/react-google-maps'
 import React from 'react'
 
-const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API!;
-const mapId = process.env.NEXT_PUBLIC_MAP_ID!;
+
+const apiKey:string = process.env.NEXT_PUBLIC_GOOGLE_MAP_API!;
+const mapId:string = process.env.NEXT_PUBLIC_MAP_ID!;
+j
 
 export default function Map2() {
   return (
     <>
         <APIProvider apiKey={apiKey}>
             <Map
+
                 style={{width:'100vw',height:'50vh'}}
                 defaultCenter={{lat:-26.974673114466885,lng:26.66995356468627}}
                 defaultZoom={15}
